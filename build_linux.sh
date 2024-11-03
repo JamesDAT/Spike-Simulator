c6f65df4ad8ffe5e8c51270e0f7fd9d900bffb32
JRH3221 <144601613+JamesDAT@users.noreply.github.com>
2024-11-03 21:24:04 +1100

    Moved linux build to make

--- build_linux.sh
+++ build_linux.sh
@@ -8,8 +8,8 @@ mkdir -p build/linux
 # Navigate to the build directory
 cd build/linux
 
-# Run CMake and build the project
-cmake -G "Ninja" -DCMAKE_CXX_COMPILER=clang++ ../..
+# Run CMake with Make as the generator
+cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ ../..
 if [ $? -ne 0 ]; then
     echo "CMake configuration failed!"
     exit 1
